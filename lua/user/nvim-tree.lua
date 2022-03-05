cc41bef5f03e591532f939fde6b6d5de20ad9367
Hamza Mughal <hamza@prodesquare.com>
2022-03-06 03:20:35 +0500

    nvim-tree integration

---
+++ lua/user/nvim-tree.lua
@@ -0,0 +1,39 @@
+-- nvim tree
+
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
+
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
+nvim_tree.setup {
+  view = {
+    mappings = {
+      list = {
+        { key = { "l", "<CR>", "o" }, action = "edit" },
+        { key = "h", action = "close_node" },
+        { key = "v", action = "vsplit" },
+      },
+    },
+  },
+}
